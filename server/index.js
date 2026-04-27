8aed1dfe925d40e65a03b6079f71862ff9a69290
vivek <codecrafter67@gmail.com>
2026-04-27 14:09:11 +0530

    set server port to 4000, add Dockerfile and .dockerignore

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get("/api/message",(req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,"0.0.0.0",()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
